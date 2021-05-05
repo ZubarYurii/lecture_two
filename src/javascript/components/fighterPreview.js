8b89492d1728c217d539b2ed29ab1a550c038603
ErihKoh <zubar.yurii1983@gmail.com>
2021-05-05 21:53:37 +0300

    start project

---
+++ src/javascript/components/fighterPreview.js
@@ -0,0 +1,33 @@
+import { createElement } from '../helpers/domHelper';
+import fighterService from '../services/fightersService'
+
+export function createFighterPreview(fighter, position) {
+  const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+  // const {image, name, health} = fighter 
+  const fighterElement = createElement({
+    tagName: 'div',
+    className: `fighter-preview___root ${positionClassName}`,
+  });
+
+  // todo: show fighter info (image, name, health, etc.)
+  
+
+
+  return fighterElement;
+}
+
+export function createFighterImage(fighter) {
+  const { source, name } = fighter;
+  const attributes = { 
+    src: source, 
+    title: name,
+    alt: name 
+  };
+  const imgElement = createElement({
+    tagName: 'img',
+    className: 'fighter-preview___img',
+    attributes,
+  });
+
+  return imgElement;
+}
